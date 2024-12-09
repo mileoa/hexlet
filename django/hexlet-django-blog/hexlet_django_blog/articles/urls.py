c29dd90e229b06265dce6b615884c215e740e988
mileoa <ezkimozzzzz@gmail.com>
2024-12-09 19:57:08 +1000

    feature: add article_show

--- django/hexlet-django-blog/hexlet_django_blog/articles/urls.py
+++ django/hexlet-django-blog/hexlet_django_blog/articles/urls.py
@@ -3,4 +3,5 @@ from hexlet_django_blog.articles import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="article"),
+    path("<int:id>/", views.ArticleView.as_view(), name="article_show"),
 ]
--- django/hexlet-django-blog/hexlet_django_blog/articles/views.py
+++ django/hexlet-django-blog/hexlet_django_blog/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from hexlet_django_blog.articles.models import Article
 from django.views import View
@@ -13,5 +13,18 @@ class IndexView(View):
             "articles/index.html",
             context={
                 "articles": articles,
+            },
+        )
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs["id"])
+        return render(
+            request,
+            "articles/show.html",
+            context={
+                "article": article,
             },
         )
